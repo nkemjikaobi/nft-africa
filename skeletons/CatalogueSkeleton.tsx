eab621a76078df2a5185b2297cf667c66ad8b10e
Obi Nkemjika Derick <obinkemjikaderick@Administrators-MacBook-Pro.local>
2022-03-28 19:04:01 +0100

    Catalogue skeleton

--- skeletons/CatalogueSkeleton.tsx
+++ skeletons/CatalogueSkeleton.tsx
@@ -6,7 +6,7 @@ import { range } from "lodash";
 const CatalogueSkeleton = () => {
   return (
 		<div className='grid p-5 tablet:p-5 desktop:grid-cols-[600px_minmax(900px,_1fr)_100px] gap-6 tablet:flex tablet:items-center tablet:flex-col laptop:grid'>
-			<div className='row-span-3 relative h-full laptop:mx-auto z-10 hover:scale-105 cursor-pointer hover:z-20 transition duration-700 ease-in-out'>
+			<div className='row-span-3 relative h-full laptop:mx-auto z-10  cursor-pointer'>
 				<Skeleton
 					width={600}
 					height={620}
@@ -15,7 +15,7 @@ const CatalogueSkeleton = () => {
 			<div className='grid grid-cols-3 gap-4'>
 				{range(6).map((data, index) => (
 					<div
-						className='relative z-10 hover:scale-110 cursor-pointer hover:z-20 transition duration-700 ease-in-out'
+						className='relative z-10 cursor-pointer'
 						key={index}
 					>
 						<Skeleton
