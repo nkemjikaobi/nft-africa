b2d15471710bb58e6f3c479180614825f80386fe
Obi Nkemjika Derick <obinkemjikaderick@Administrators-MacBook-Pro.local>
2022-04-12 22:35:10 +0100

    Add infura node for guests

--- components/Catalogue/Catalogue.tsx
+++ components/Catalogue/Catalogue.tsx
@@ -11,7 +11,7 @@ const Catalogue = () => {
 	useEffect(() => {
 		let mounted = true;
 		if (mounted) {
-			setTimeout(() => setLoading(false), 2000);
+			setTimeout(() => setLoading(false), 1000);
 		}
 
 		return () => {
